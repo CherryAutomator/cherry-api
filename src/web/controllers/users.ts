b9fe50ee05ac34b641f81c4cf478689f7dfa0640
Gustavo Barca <gustavogb2@gmail.com>
2021-11-25 19:56:13 -0300

    feat: create enpoint 'me'

--- src/web/controllers/users.ts
+++ src/web/controllers/users.ts
@@ -1,8 +1,10 @@
 import { UserService } from "../../application/services/UserService";
-import { Post } from "../utils/decorators";
+import { Get, Post } from "../utils/decorators";
 import { Response } from "../utils/response";
 import { Request } from "express";
 import { error } from "../utils/errors";
+import { getUserId } from "../utils/params";
+import { format } from "../utils/mapper";
 
 export class UsersController {
   constructor(private readonly userService: UserService) { }
@@ -13,6 +15,17 @@ export class UsersController {
       const tokens = await this.userService.create(req.body);
 
       res.send({ content: tokens, message: 'User created successfull' });
+    } catch (err) {
+      return error(err, res);
+    }
+  }
+
+  @Get('/me')
+  async getMe(_req: Request, res: Response<any>) {
+    try {
+      const user = await this.userService.getOne(getUserId(res));
+
+      res.send({ content: format.user(user), message: 'User created successfull' });
     } catch (err) {
       return error(err, res);
     }
